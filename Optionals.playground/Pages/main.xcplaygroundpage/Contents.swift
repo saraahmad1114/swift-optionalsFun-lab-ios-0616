de67bc0261ecbfcb877482c018930fd10c0504f8
Sara <saraahmad1114@yahoo.com>
2016-07-08 10:27:33 -0400

    optional exercises

--- Optionals.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Optionals.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@
  ### 1. What is the type of the variable `pet` declared below?
  */
 var pet = "turtle 🐢"
-
+//its a string type
 
 
 
@@ -23,8 +23,7 @@ var pet = "turtle 🐢"
  ### 2. What is the type of the variable `petName` declared below?
  */
 var petName: String?
-
-
+//an optional of string type
 
 
 
@@ -32,7 +31,8 @@ var petName: String?
 /*: question3
  ### 3. Currently, what is the _value_ of `petName`?
  */
-
+print("\(petName)")
+//its goint to be equal to nil because i haven't yet unwrapped it
 
 
 
@@ -44,7 +44,10 @@ var petName: String?
  */
 // write your code here
 
-
+if let pet = petName
+{
+    print("My pets name is \(petName)")
+}
 
 
 
@@ -55,7 +58,8 @@ var petName: String?
  */
 // write your code here
 
-
+print("\(petName)")
+//should equal to nil
 
 
 
@@ -65,7 +69,11 @@ var petName: String?
  ### 6. Write an if statement that only prints `petName`'s value if it is not `nil`. If `petName` is `nil`, print, "There is no pet name." What do you expect to see in the console?
  */
 // write your code here
-
+if petName != nil {
+    print("\(petName)")
+} else {
+    print("There is no pet name.")
+}
 
 
 
@@ -77,7 +85,10 @@ var petName: String?
  */
 // write your code here
 
-
+if let pet = petName
+{
+    print("\(petName)")
+}
 
 
 
@@ -88,7 +99,9 @@ var petName: String?
  */
 var anotherPetName: String?
 // write your code here
-
+if anotherPetName == nil {
+    print("The value of anotherPetName is nil")
+}
 
 
 
@@ -99,22 +112,23 @@ var anotherPetName: String?
  ### 9. Using optional binding, write an if statement that prints "anotherPetName has no value" if it is `nil`; otherwise, print the _unwrapped_ value of `anotherPetName`. What do you expect to see in the console?
  */
 // write your code here
-
-
-
-
-
-
-
-
+if let pet = anotherPetName
+{
+    print("\(pet)")
+}
+else if anotherPetName == nil
+{
+    print("\(anotherPetName)")
+}
 
 /*: question10
  ### 10. Declare a _constant_ optional string (using the `let` keyword). Then try to assign a string value to it on the next line. Print out the constant. What happens?
  */
 // write your code here
 
-
-
+let name: String?
+name = "Sara"
+print("\(name)")
 
 
 
@@ -126,7 +140,8 @@ var anotherPetName: String?
  */
 // write your code here
 
-
+let firstName: String? = nil
+print ("\(firstName)")
 
 
 
